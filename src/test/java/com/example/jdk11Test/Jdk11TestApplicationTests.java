141283e6fc3fda34709471498d1fe79b0d517d23
gusdn7142 <gusdnchl7144@naver.com>
2025-02-01 23:42:04 +0900

    first commit

---
+++ src/test/java/com/example/jdk11Test/Jdk11TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdk11Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk11TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
